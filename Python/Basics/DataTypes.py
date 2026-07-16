401536874036ee64c837c65a6dce29e47eba4d9d
Purushotham Reddy <purushothamadf452@gmail.com>
2026-07-16 13:35:50 +0530

    My First Python File push to repo

---
+++ Python/Basics/DataTypes.py
@@ -0,0 +1,8 @@
+Name = "Purushotham" # This is a string data type
+
+Age = 36 # This is an integer data type
+Course = "Azure Data Engineer" # This is a string data type
+
+print("Name:{}, Age:{}, Course:{}".format(Name, Age, Course))
+
+print(f"My self {Name}, and I am {Age} years old, pursuing the {Course} course.")
